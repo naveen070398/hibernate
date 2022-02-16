ff0a0452202c4003a48ad318e32ad3525c47f5b8
naveenkumar070398 <nbodakunti@mondee.com>
2022-02-16 08:05:42 +0530

    m

---
+++ Hibernatejoinedclass/src/com/mondee/CreditCard.java
@@ -0,0 +1,14 @@
+package com.mondee;
+
+public class CreditCard extends Payment {
+	private String CreditCardType;
+ 	
+	 public String getCreditCardType() 
+	 {
+	 	return CreditCardType;
+	 }
+	 public void setCreditCardType(String CreditCardType)
+	 {
+	 	this.CreditCardType=CreditCardType;
+	 }
+}
